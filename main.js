0c77844fad67ac98ae0964ad84bd4fa71715a376
Chad Dore <chaduke@gmail.com>
2023-10-03 20:38:17 -0500

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-const ctx = document.getElementById("canvas").getContext("2d");
+window.onload=function() {
+    var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
 
-ctx.globalCompositeOperation = "destination-over";
-ctx.clearRect(0, 0, 800, 600); // clear canvas
+    var centerX = width / 2,
+    centerY = height / 2,
+    xRadius = 300,
+    yRadius = 300,
+    xangle = 4,
+    yangle = 4,
+    xspeed = 0.04,
+    yspeed = 0.04,
+    x,y;
+
+    render();
+
+    function render() {
+        
+        x = centerX + Math.cos(xangle) * xRadius, 
+        y = centerY + Math.sin(yangle) * yRadius;
+        
+        context.fillStyle = "deepskyblue";
+        context.fillRect(0,0,width,height);
+        context.beginPath();
+        context.arc(x,y,25,0,Math.PI * 2,false);
+        context.fillStyle="yellow";
+        context.fill();
+        xangle += xspeed;
+        yangle += yspeed;
+        requestAnimationFrame(render);
+    }
+};
